d3f0f893f5df178fde8ae9b2be1298fb0961217d
Alec Iberg <ibergalec@gmail.com>
2021-05-10 20:59:35 -0700

    reorganized

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const readline = require("readline");
 const rp = require("request-promise");
 const fetch = require("node-fetch");
 
+let more = true;
 async function takeInput() {
   const rl = readline.createInterface({
     input: process.stdin,
@@ -11,39 +12,25 @@ async function takeInput() {
 
   rl.question("What Coin Would You Like To Search For? ", async (answer) => {
     console.log(`Searching for ${answer}...`);
-    const value = await getCoin(answer);
+    const value = await getCoin(answer).then((res) => {
+      const { price } = res.data[answer.toUpperCase()].quote.USD;
+      return Math.round(price * 100) / 100;
+    });
     console.log(`The price of ${answer} is currently ${value}`);
   });
+
   rl.on("line", (input) => {
-    console.log("Closing....");
     rl.close();
   });
 }
 
-// // working
-// async function getCoin(query) {
-//     const apiKey = "b9968c2b-c365-457e-b409-f39d7bdb0acd";
-//     const url =
-//       "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest";
-//     let qString = "?CMC_PRO_API_KEY=" + apiKey + `&symbol=${query.toUpperCase()}`;
-//     const response = await fetch(url + qString);
-//     const json = await response.json();
-//     const price = json.data[query.toUpperCase()].quote.USD.price;
-//     return Math.round(price * 100) / 100;
-//   }
-
-// refactor
 async function getCoin(query) {
   const apiKey = "b9968c2b-c365-457e-b409-f39d7bdb0acd";
   const url =
     "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest";
   let qString = "?CMC_PRO_API_KEY=" + apiKey + `&symbol=${query.toUpperCase()}`;
-  const response = await fetch(url + qString)
-    .then((res) => await res.json())
-    .then((json) => {
-      const { price } = json.data[query.toUpperCase()].quote.USD.price;
-      return Math.round(price * 100) / 100;
-    });
+  const response = await fetch(url + qString);
+  return response.json();
 }
 
 takeInput();
